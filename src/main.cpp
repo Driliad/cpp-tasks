4c061421bdda836d8c54b85abca0906ee44e052a
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2020-08-30 00:09:06 +0300

    fix: add links to 3-conditions-tasks definition files

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,3 @@
 #include "./1-numbers-tasks/1-numbers-tasks.cpp"
 #include "./2-booleans-tasks/2-booleans-tasks.cpp"
+#include "./3-conditions-tasks/3-conditions-tasks.cpp"
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,4 @@
 #define CATCH_CONFIG_MAIN
 #include "1-numbers-tasks-tests.cpp"
 #include "2-booleans-tasks-tests.cpp"
+#include "3-conditions-tasks-tests.cpp"
